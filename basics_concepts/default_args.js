37e2cebbd56cb68e01365e9601ced064d23578f0
hoodielive <hoodielive@gmail.com>
2018-06-24 21:17:55 -0400

    working with template strings and default args

---
+++ basics_concepts/default_args.js
@@ -0,0 +1,9 @@
+function add(x, y) {
+    if ( y === undefined) {
+        y = 5
+    } 
+    return x + y 
+} 
+
+console.log(add(4)) 
+
---
+++ basics_concepts/template_strings.js
@@ -0,0 +1,6 @@
+var name = 'law'
+
+console.log('Hello, my name is' + name) 
+console.log(`Hello, my name is ${name}`) 
+
+
