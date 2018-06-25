5369858209c314fa5af079355555dbe3e31d4a2e
hoodielive <hoodielive@gmail.com>
2018-06-25 19:14:16 -0400

    working with spreads

---
+++ basics_concepts/rest-spread2.js
@@ -0,0 +1,27 @@
+function talk(greeting, farewell, names) { 
+    names.forEach(function(name) {
+        console.log(`${greeting} ${name}`)
+        console.log(`${farewell} ${name}`)
+    })
+} 
+
+function talk2(greeting, farewell) {
+    console.log(arguments) 
+    var args = Array.prototype.slice.call(arguments, 2) 
+    console.log(args) 
+
+} 
+
+//talk('hello', 'goodbye', ['bill', 'joe', 'morty']);          
+talk2('hello', 'goodbye', 'bill', 'joe', 'morty');          
+
+
+function talk3(greeting, farewell, ...names) { 
+    names.forEach(function(name) {
+        console.log(`${greeting} ${name}!`)
+        console.log(`${farewell} ${name}!`)
+    })
+} 
+
+
+talk3('hello', 'goodbye', ['bill', 'joe', 'morty']);          
