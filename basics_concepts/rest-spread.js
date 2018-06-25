42385fc38c842e0a8eb4024999d4c9fc58548936
hoodielive <hoodielive@gmail.com>
2018-06-24 21:30:40 -0400

    expanding betterAdd function on rest-spread

--- basics_concepts/rest-spread.js
+++ basics_concepts/rest-spread.js
@@ -6,3 +6,14 @@ function add() {
     return result; 
 }
 console.log(add(2,4,3)) 
+
+
+function betterAdd(...values) {
+    console.log(values) 
+    var result = values.reduce(function(memo, value) {
+        return memo + value 
+    })
+    return result
+} 
+
+console.log(betterAdd(2, 4, 3))  
