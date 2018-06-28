b8faf9478c3854a4b3530b593983ae36efb324cb
hoodielive <hoodielive@gmail.com>
2018-06-28 17:16:01 -0400

    Gucci promises and shiii

--- basics_concepts/promises.js
+++ basics_concepts/promises.js
@@ -1,4 +1,7 @@
-var fs = require('fs')
-
-fs.readFile('./script.js', function(err, function() { 
-} 
+var fs = require('fs') 
+  fs.readFile('./script.js', function(err,data)  {
+    console.log("This is just so you'll know that we found script.js") 
+      fs.readFile('./pyramid.js', function(err, data) {
+        console.log("This is just so you'll know that we found pyramid.js") 
+      })
+  }) 
